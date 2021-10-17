b8df63c8caddb075deec93643abed2173955e6aa
Chlesea <1556792948@qq.com>
2021-10-17 23:28:50 +0800

    10/17

---
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.uiwidgettest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val button : Button = findViewById(R.id.button)
+//        val editText : EditText = findViewById(R.id.editText)
+//        val imageView : ImageView = findViewById(R.id.imageView)
+//        val processBar : ProgressBar = findViewById(R.id.progressBar)
+//        button.setOnClickListener {
+            //在此处添加逻辑
+//            val inputText = editText.text.toString()
+//            Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show()
+//            imageView.setImageResource(R.drawable.img_2)
+//            if (processBar.visibility == View.VISIBLE) {
+//                processBar.visibility = View.GONE
+//            } else {
+//                processBar.visibility = View.VISIBLE
+//            }
+
+//            processBar.progress = processBar.progress + 10
+
+//            AlertDialog.Builder(this).apply {
+//                setTitle("This is Dialog")
+//                setMessage("Something important")
+//                setCancelable(false)
+//                setNegativeButton("OK") { dialog, which -> }
+//                setPositiveButton("Cancel") { dialog, which -> }
+//                show()
+//            }
+
+//        }
+    }
+}
